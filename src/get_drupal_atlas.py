6ae52e266d0466822977f6bfe6297b01ae8805da
arnfinn <arnfinn.steindal@gmail.com>
2021-09-28 11:15:55 +0200

    almost good

--- src/get_drupal_atlas.py
+++ src/get_drupal_atlas.py
@@ -6,6 +6,30 @@ import markdownify
 from shutil import copy
 import glob
 
+atlas = {1: "no/barn",
+         39: "no/dagkir",
+         49: "no/nyfodt",
+         90: "no/eldre",
+         117: "en/dagkir",
+         118: "en/barn",
+         120: "en/nyfodt",
+         121: "en/eldre",
+         123: "no/kols",
+         131: "en/kols",
+         138: "no/ortopedi",
+         150: "no/dagkir2",
+         154: "en/dagkir2",
+         155: "no/gyn",
+         157: "no/fodsel",
+         158: "en/gyn",
+         178: "en/fodsel",
+         184: "en/ortopedi",
+         259: "no/psyk",
+         260: "no/kvalitet",
+         274: "en/psyk",
+         279: "en/kvalitet"
+         }
+
 def map_hovedfunn_atlas(file_content):
   for line in file_content:
      if re.search("atlas-button atlas", line):
@@ -23,6 +47,7 @@ def get_hovedfunn_title(file_content):
 def get_hovedfunn_content(file_content, img_path):
     html_content = ""
     collecting = False
+    mod_img_path = img_path.replace("output/", "/helseatlas/")
     for line in file_content:
         # Start collecting text from line with BEGIN OUTPUT ...
         if re.search("BEGIN OUTPUT from 'sites/all/themes/custom/helseatlas/templates/ds-1col", line):
@@ -34,9 +59,7 @@ def get_hovedfunn_content(file_content, img_path):
             if re.search("media-element file-default", line):
                 # Replace image line
                 filename = line.split()[1].split("/")[-1].replace("\"", "")
-                html_content = html_content + "![" + filename.split(".")[-2] + "](" + img_path + "/" + filename + ")"
-#            elif re.search("Nedlastbar PDF", line):
-#                pass
+                html_content = html_content + "![" + filename.split(".")[-2] + "](" + mod_img_path + "/" + filename + ")"
             elif re.search("element-invisible", line):
                 pass
             else:
@@ -64,27 +87,47 @@ def get_hovedfunn_order(file_content):
             hovedfunn.append("hovedfunn/" + line.replace("%C3%B8", "ø").replace("%C3%A6", "æ").replace("%C3%A5", "å").replace("%C2%A0", "").split(">")[4].split("/")[-1].replace("\"", ""))
     return(hovedfunn)
 
+def get_hovedfunn_start(file_content):
+    count = 0
+    return_string = ""
+    for line in file_content:
+        if count == 1:
+            return_string = return_string + line + "\n"
+            if re.search("</div>", line):
+               count = count + 1
+        if re.search("<div class=\"pane-content\">", line):
+            count = count + 1
+    
+    return(markdownify.markdownify(return_string.strip(), heading_style = "ATK"))
+
+if not os.path.exists("output/md/no"):
+    os.makedirs("output/md/no")
+if not os.path.exists("output/md/en"):
+    os.makedirs("output/md/en")
+
 for details in glob.glob("atlas/*/details"):
+    atlas_num = int(details.split("/")[1])
+    atlas_name = atlas[atlas_num]
+
+    img_path = "output/img/" + atlas_name
+    md_name = "output/md/" + atlas_name + ".md"
+    if not os.path.exists(img_path):
+        os.makedirs(img_path)
 
     detailsfile_content = get_file_content(details)
     entries = get_hovedfunn_order(detailsfile_content)
+    hovedfunn_start = get_hovedfunn_start(detailsfile_content)
+
+    md_file = open(md_name, "w")
+    md_file.write(hovedfunn_start)
+    md_file.close()
 
     for entry in entries:
         try:
             file_content = get_file_content(entry)
-            atlas_num = map_hovedfunn_atlas(file_content)
-
-            img_path = "output/img/" + str(atlas_num)
-            md_path = "output/md/" + str(atlas_num)
-
-            if not os.path.exists(md_path):
-                os.makedirs(md_path)
-            if not os.path.exists(img_path):
-                os.makedirs(img_path)
-
             title_line = get_hovedfunn_title(file_content)
             hovedfunn = get_hovedfunn_content(file_content, img_path)
-            md_file = open(md_path + "/hovedfunn.md", "a")
+            md_file = open(md_name, "a")
             md_file.write(hovedfunn)
             md_file.close()
 
